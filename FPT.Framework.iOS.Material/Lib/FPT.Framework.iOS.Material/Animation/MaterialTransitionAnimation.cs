7cab444b77cd6da79fe82472b6ed4016527bc9fe
phamquan <mr.pquan@gmail.com>
2016-09-26 10:48:10 +0700

    add Material Basic Animation

--- FPT.Framework.iOS.Material/Lib/FPT.Framework.iOS.Material/Animation/MaterialTransitionAnimation.cs
+++ FPT.Framework.iOS.Material/Lib/FPT.Framework.iOS.Material/Animation/MaterialTransitionAnimation.cs
@@ -1,6 +1,35 @@
 ﻿using System;
+using Foundation;
+using CoreAnimation;
 namespace FPT.Framework.iOS.Material
 {
+	public class MaterialAnimationTransitionType
+	{
+		private NSString Value = NSString.Empty;
+		public static implicit operator NSString(MaterialAnimationTransitionType ts)
+		{
+			return ((ts == null) ? null : ts.Value);
+		}
+		public static implicit operator MaterialAnimationTransitionType(NSString val)
+		{
+			return new MaterialAnimationTransitionType { Value = val };
+		}
+
+		public static implicit operator string(MaterialAnimationTransitionType ts)
+		{
+			return ((ts == null) ? null : ts.ToString());
+		}
+	}
+
+	public static class MaterialAnimationTransition
+	{
+		public static readonly MaterialAnimationTransitionType Fade = CATransition.TransitionFade;
+		public static readonly MaterialAnimationTransitionType MoveIn = CATransition.TransitionMoveIn;
+		public static readonly MaterialAnimationTransitionType Push = CATransition.TransitionPush;
+		public static readonly MaterialAnimationTransitionType Reveal = CATransition.TransitionReveal;
+	}
+
+
 	public class MaterialTransitionAnimation
 	{
 		public MaterialTransitionAnimation()
