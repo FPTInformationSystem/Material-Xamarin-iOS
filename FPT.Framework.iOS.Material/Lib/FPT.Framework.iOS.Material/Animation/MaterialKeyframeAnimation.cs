10c4b82e308c4fb9b241a4a120407e75ef4e4eef
phamquan <mr.pquan@gmail.com>
2016-09-27 10:47:40 +0700

    add key frame and pulse

--- FPT.Framework.iOS.Material/Lib/FPT.Framework.iOS.Material/Animation/MaterialKeyframeAnimation.cs
+++ FPT.Framework.iOS.Material/Lib/FPT.Framework.iOS.Material/Animation/MaterialKeyframeAnimation.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using Foundation;
+using CoreAnimation;
+using UIKit;
+
 namespace FPT.Framework.iOS.Material
 {
-	public class MaterialKeyframeAnimation
+	public static partial class Convert
 	{
-		public MaterialKeyframeAnimation()
+		public static string MaterialAnimationRotationModeToValue(MaterialAnimationRotationMode mode)
 		{
+			switch (mode)
+			{
+				case MaterialAnimationRotationMode.None:
+					return null;
+				case MaterialAnimationRotationMode.Auto:
+					return CAAnimation.RotateModeAuto;
+				case MaterialAnimationRotationMode.AutoReverse:
+					return CAAnimation.RotateModeAutoReverse;
+				default:
+					return null;
+			}
+		}
+	}
+
+	public enum MaterialAnimationRotationMode
+	{
+		None, Auto, AutoReverse
+	}
+
+	public static partial class MaterialAnimation
+	{
+		public static CAKeyFrameAnimation Path(UIBezierPath bezierPath, MaterialAnimationRotationMode mode = MaterialAnimationRotationMode.Auto, double? duration = null)
+		{
+			var animation = CAKeyFrameAnimation.FromKeyPath("position");
+			animation.Path = bezierPath.CGPath;
+
+			animation.RotationMode = Convert.MaterialAnimationRotationModeToValue(mode);
+			if (duration != null)
+				animation.Duration = duration.Value;
+			return animation;
 		}
 	}
 }
