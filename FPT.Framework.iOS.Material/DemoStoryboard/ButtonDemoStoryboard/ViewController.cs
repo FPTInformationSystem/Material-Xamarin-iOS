350f8c7b981506c69e64c1e45864c5a4a488b626
phamquan <mr.pquan@gmail.com>
2016-12-09 18:18:13 +0700

    add material button storyboard demo

---
+++ FPT.Framework.iOS.Material/DemoStoryboard/ButtonDemoStoryboard/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FPT.Framework.iOS.Material;
+using UIKit;
+
+namespace ButtonDemoStoryboard
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			RaisedButton.SetTitle(title: "Button Raised", forState: UIControlState.Normal);
+			RaisedButton.SetTitleColor(color: Color.White, forState: UIControlState.Normal);
+			RaisedButton.PulseColor = Color.White;
+			RaisedButton.TitleLabel.Font = RobotoFont.RegularWithSize(24f);
+			RaisedButton.BackgroundColor = Color.Blue.Base;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
---
+++ FPT.Framework.iOS.Material/DemoStoryboard/ButtonDemoStoryboard/ViewController.designer.cs
@@ -0,0 +1,26 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ButtonDemoStoryboard
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		FPT.Framework.iOS.Material.RaisedButton RaisedButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (RaisedButton != null) {
+				RaisedButton.Dispose ();
+				RaisedButton = null;
+			}
+		}
+	}
+}
